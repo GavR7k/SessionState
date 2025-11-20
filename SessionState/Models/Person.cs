09a0e66cb10a6c90926a14441fab3a98c8f37df4
gavr7k <gavrilovich2002@gmail.com>
2025-11-20 21:40:30 +0300

    Тест работы с сессиями

---
+++ SessionState/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SessionState.Models
+{
+    public class Person
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age {  get; set; }
+
+    }
+}
--- SessionState/Program.cs
+++ SessionState/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(1); // установка времени сессии
+});
+
+builder.Services.AddControllersWithViews();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
+app.UseRouting();
+app.UseSession();
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
